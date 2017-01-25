4002cff86643b29fdabb1cca585b236127668450
Louremy Pena <louremypena@wifi_guest-10-38-100-234.uncc.edu>
2017-01-24 21:01:27 -0500

    created the ajax function for the text analytics api and added the script src links to the index.html

---
+++ textapi.js
@@ -0,0 +1,24 @@
+
+    $(function() {
+        var params = {
+            // Request parameters
+        };
+      
+        $.ajax({
+            url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","{63216139951246bbb219cbec04e4e18d}");
+            },
+            type: "POST",
+            // Request body
+            data: "{body}",
+        })
+        .done(function(data) {
+            alert("success");
+        })
+        .fail(function() {
+            alert("error");
+        });
+    });
