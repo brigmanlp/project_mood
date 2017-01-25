df41615e137c67ab83f345f59ffbca12792c4c26
ByronDaleRichardson <musicmanx@gmail.com>
2017-01-25 11:55:01 -0500

    added .ajax & var for video link

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,6 +1,20 @@
+//This var will hold the search paramater for each mood
 var seach = $("currentmood").val();
 
+//This is the queryURL for the ajax 
 var queryURL = 
 	"http://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=relevance&q=" + search + "&safeSearch=moderate&type=video&videoDefinition=standard&videoDuration=short&videoEmbeddable=true&key=AIzaSyCGMvVG5K-LtfM2yacVG4hiPz3xp9zNCZc"  
 
-	
+//ajax to get response from queryURL
+$.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).done(function(response) {
+      console.log(response);
+    });
+    
+//This will be video ID from returned array for next step
+var videoID = //id from returned array
+
+//This link will go to video
+var videoLink =	"https://www.youtube.com/watch?v=" + videoID ;
