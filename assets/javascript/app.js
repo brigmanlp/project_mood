a9c70f4a93697ce06d2c7e9f899678f20b349f0b
Matt White <mattwhite1337@gmail.com>
2017-01-28 12:49:39 -0500

    fix for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -263,7 +263,7 @@ database.ref('users/').on('value', function(snapshot){
 		console.log(snapshot.val()[firebase.auth().currentUser.uid]);
     var keys = Object.keys(snapshot.val()[firebase.auth().currentUser.uid]);
     for (var i = 0; i < keys.length; i++){
-      console.log(snapshot.val()[firebase.auth().currentUser][keys[i]]);
+      console.log(snapshot.val()[firebase.auth().currentUser.uid][keys[i]]);
     }
 	}
 });
