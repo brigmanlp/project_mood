0e8264e8744b9f0cc20a8fde45110615957c3b20
Matt White <mattwhite1337@gmail.com>
2017-01-28 23:06:02 -0500

    adding rolling 7 days

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -256,8 +256,14 @@ database.ref('users/').on('value', function(snapshot){
 	if(firebase.auth().currentUser !== null){
 		console.log(snapshot.val()[firebase.auth().currentUser.uid]);
     var keys = Object.keys(snapshot.val()[firebase.auth().currentUser.uid]);
+
+    if(keys.length >= 7){
+      keys = keys.slice(keys.length - 7);
+    }
+
     for (var i = 0; i < keys.length; i++){
       console.log(snapshot.val()[firebase.auth().currentUser.uid][keys[i]]);
+      barChartData.labels.push(snapshot.val()[firebase.auth().currentUser.uid][keys[i]].moment);
       barChartData.datasets[0].data.push(snapshot.val()[firebase.auth().currentUser.uid][keys[i]].response);
     }
     displayChart();
